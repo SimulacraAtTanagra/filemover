73d016e051a13d8e502fb204df36ec5e7b5eeb02
SimulacraAtTanagra <shaneayers1@gmail.com>
2021-03-25 18:27:36 -0400

    updated filemover.py

--- filemover.py
+++ filemover.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Mar 25 15:57:57 2021
-
-@author: shane
-"""
-
 import pathlib
 import os
 import datetime
@@ -16,27 +9,63 @@ def grab_dates(file):
     create = datetime.datetime.fromtimestamp(fname.stat().st_ctime).year
     modify = datetime.datetime.fromtimestamp(fname.stat().st_mtime).year
     if create==modify:
-        return(create)
+        return(str(create))
     elif modify<create:
-        return(modify)
+        return(str(modify))
     else:
-        return(modify)
+        return(str(modify))
         
 def foldercheck(folderstring):
     if os.path.isfile(folderstring):
         return(True)
     else:
-        os.mkdir(folderstring)
-        return(foldercheck(folderstring))
+        try:
+            os.mkdir(folderstring)
+            foldercheck(folderstring)
+        except FileExistsError:
+            return(True)
+        
+def try_move(filename,dest):
+    filename=os.path.abspath(filename)
+    dest=os.path.abspath(dest)
+    try:
+        shutil.move(filename,dest)
+    except:
+        filelist=filename.split('.')
+        filenew='.'.join(filelist[:-1])+"X."+filelist[-1]
+        filenew=os.path.abspath(filenew)
+        try:
+            os.rename(filename,filenew)
+        except:
+            os.rename(filename,filenew)
+        try_move(filename,dest)
         
 def file_mover(folderstring):
-    filelist= os.listdir(folderstring)
-    dates=[grab_dates(file) for file in os.listdir(folderstring)]
+    filelist= [file for file in os.listdir(folderstring) if "." in file]
+    dates=[grab_dates(os.path.join(folderstring,file)) for file in filelist]
     dates=list(set(dates))
     for date in dates:
         foldercheck(os.path.join(folderstring,date))
     for file in filelist:
         filefolder=os.path.join(folderstring,grab_dates(os.path.join(folderstring,file)))
-        shutil.move(os.path.join(folderstring,file),filefolder)
+        #shutil.move(os.path.join(folderstring,file),filefolder)
+        try_move(os.path.join(folderstring,file),filefolder)
+def file_count(folder):
+    counter=0
+    for dir,subdir, files in os.walk(folder):
+        for file in files:
+            counter+=1
+    return(counter)
     
-    
+def file_dupes(folder):
+    filelist=[]
+    for dir, subdir, files in os.walk(folder):
+        for file in files:
+            filelist.append(file)
+    filelist=list(set(filelist))
+    registration={file:[] for file in filelist}
+    for dir, subdir, files in os.walk(folder):
+        for file in files:
+            registration[file].append(os.path.join(dir,file))
+    print(len([i for i in registration.values() if len(i)>1]),"duplicates")
+    return(registration)
